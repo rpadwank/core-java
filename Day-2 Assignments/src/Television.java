0251062a7258ca61f795afe06f193b69a629b48c
Padwankar <ruchir-shailesh.padwankar@capgemini.com>
2019-03-09 15:58:37 +0530

    modified TelevisionTest

--- Day-2 Assignments/src/Television.java
+++ Day-2 Assignments/src/Television.java
@@ -25,29 +25,38 @@ class Television
 		channel++;
 		if(channel>300)
 			channel = 0;
+		System.out.println("increase channel");
+		System.out.println("current channel: "+channel+"\n");
 	}
 	public void decreaseChannel()
 	{
 		channel--;
 		if(channel<0)
 			channel = 300;
+		System.out.println("decrease channel");
+		System.out.println("current channel: "+channel+"\n");
 	}
 	public void increaseVolume()
 	{
 		volume++;
 		if(volume>50)
 			volume = 50;
+		System.out.println("increase volume");
+		System.out.println("current volume: "+volume+"\n");
 	}
 	public void decreaseVolume()
 	{
 		volume--;
 		if(volume<0)
 			volume = 0;
+		System.out.println("decrease volume");
+		System.out.println("current volume: "+volume+"\n");
 	}
 	public void displayInformation()
 	{
+		System.out.println("\nCurrent State:");
 		System.out.println("Power State: "+state);
 		System.out.println("Volume: "+volume);
-		System.out.println("Channel: "+channel);		
+		System.out.println("Channel: "+channel+"\n");		
 	}
 }
--- Day-2 Assignments/src/TelevisionTest.java
+++ Day-2 Assignments/src/TelevisionTest.java
@@ -3,6 +3,7 @@ class TelevisionTest
 	public static void main(String args[])
 	{
 		Television tv = new Television("OFF",20,1);
+		tv.displayInformation();
 		tv.turnOnOff();
 		tv.increaseVolume();
 		tv.increaseVolume();
